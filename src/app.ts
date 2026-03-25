0ab4aa26634bc8a93f133b483a48f42426650841
Tuomas Rinne <tuomas.rin@hotmail.com>
2026-03-25 16:50:04 +0200

    MB-TODO express middleware

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,15 @@ import router from './routes.ts'
 const app = express()
 
 // MB-TODO: What are middlewares in Express?
+// Middlewares allow intercepting incoming requests and outgoing responses and running arbitary code based on their contents before passing them to the next handler.
 // MB-TODO: What these middlewares do?
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet())
-app.use(morgan('tiny'))
+app.use(express.json()) // Allows reading request bodies with JSON content type
+app.use(express.urlencoded({ extended: true })) // Allows reading request bodies with urlencoded content type
+app.use(helmet()) // Sets security related headers to response
+app.use(morgan('tiny')) // Logs incoming requests
 
 app.use(router)
 
-app.use(errorHandler)
+app.use(errorHandler) // Handles errors thrown in above middlewares
 
 export default app
